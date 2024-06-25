1971e62aa007fb5389c907659193706f5e0193e5
JeffMenca <68795220+JeffMenca@users.noreply.github.com>
2024-06-24 21:50:59 -0600

    Main(Feature): Exercise 1 - MovieList on React

---
+++ pruebabi1/src/MovieList.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const MovieList = () => {
+  const fetchMovies = async () => {
+    let year = document.querySelector("input").value;
+    try {
+      const response = await axios.get(
+        `https://jsonmock.hackerrank.com/api/movies?Year=${year}`
+      );
+      setMovies(response.data.data);
+      console.log(response.data.data);
+    } catch (error) {
+      setError("No movies found");
+    }
+  };
+  const [year, setYear] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState("");
+
+  return (
+    <div className=" h-full">
+      <header className="App-header">
+        <div class="layout-column align-items-center mt-50">
+          <section className="flex justify-center">
+            <input
+              type="number"
+              className="bg-gray-200 px-6 py-2 text-center rounded-md mr-6"
+              placeholder="Enter Year eg 2015"
+              data-test-id="app-input"
+            />
+            <button
+              className="bg-green-500 px-6 py-2 text-center rounded-md text-white"
+              data-test-id="submit-button"
+              onClick={fetchMovies}
+            >
+              Search
+            </button>
+          </section>
+
+          <ul class="mt-50 styled" data-test-id="movieList">
+            {movies.length > 0 &&
+              movies.map((movie) => (
+                <li className="border-solid border-2 border-gray-100 py-4 shadow-md my-6" key={movie.imdbID}>
+                  {movie.Title}
+                </li>
+              ))}
+
+            {movies.length === 0 && (
+              <li class="slide-up-fade-in py-10">No results found</li>
+            )}
+          </ul>
+
+          <div class="mt-50 slide-up-fade-in" data-test-id="no-result"></div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default MovieList;
